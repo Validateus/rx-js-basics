163b6bd53c93dfe21e1eeafb69af73940596a781
Paul <validateus@yahoo.com>
2019-10-06 10:47:58 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Subscription, Subject} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,64 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'rx-js';
+
+  sub: Subscription
+
+  stream$: Subject<number> = new Subject<number>()
+  counter = 0
+
+  constructor() {
+
+    // const intervalStream$ = interval(1000)
+    //
+    // this.sub = intervalStream$
+    //   .pipe(
+    //     filter(value => value % 2 === 0),
+    //     map((value) => `Mapped value ${value}`)
+    //   )
+    //   .subscribe((value) => {
+    //   console.log(value)
+    // })
+
+    // const stream$ = new Observable(observer => {
+    //
+    //   setTimeout(() => {
+    //     observer.next(1)
+    //   }, 1500)
+    //
+    //   setTimeout(() => {
+    //     observer.complete()
+    //   }, 2100)
+    //
+    //   setTimeout(() => {
+    //     observer.error('Something went wrong')
+    //   }, 2000)
+    //
+    //   setTimeout(() => {
+    //     observer.next(2)
+    //   }, 2500)
+    //
+    // })
+    //
+    // stream$
+    //   .subscribe(
+    //     value => console.log('Next ', value),
+    //     error => console.log('Error ', error),
+    //     () => console.log('Complete')
+    //   )
+
+    this.sub = this.stream$.subscribe(value => {
+      console.log('Subscribe', value)
+    })
+  }
+
+  stop() {
+    this.sub.unsubscribe()
+  }
+
+  next() {
+    this.counter++
+    this.stream$.next(this.counter)
+  }
+
 }
